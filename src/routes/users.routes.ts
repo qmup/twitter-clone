0951e5ee03636696ecf14cd1cc9aaf7b0a161d52
Quan Nguyen <quannm@codehq.nz>
2023-08-11 14:49:01 +0700

    Create User Schema/ Connect User Collection

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express';
-import { loginController } from '~/controllers/users.controllers';
+import { loginController, registerController } from '~/controllers/users.controllers';
 import { loginValidator } from '~/middlewares/users.middlewares';
 
 const usersRouter = Router();
 
-usersRouter.use((req, res, next) => {
-  console.log('Time 1: ', Date.now());
-  next();
-});
-
-usersRouter.get('/tweet', (req, res) => {
-  res.send('This is tweet!');
-});
-
 usersRouter.post('/login', loginValidator, loginController);
+usersRouter.post('/register', registerController);
 
 export default usersRouter;

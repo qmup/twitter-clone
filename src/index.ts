c70cb7b19a15a2721df9304e4af9d962afbc72f7
Quan Nguyen <quannm@codehq.nz>
2023-09-09 10:32:21 +0700

    Draft send message through socket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import cors from 'cors';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 // import '~/utils/fake';
 import { UPLOAD_VIDEO_DIR } from './constants/dir';
 import { defaultErrorHandler } from './middlewares/error.middlewares';
@@ -14,6 +17,37 @@ import { initFolder } from './utils/file';
 
 const port = 4000;
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000' // Replace with your React client's URL
+  }
+});
+
+const users: {
+  [key: string]: { socket_id: string };
+} = {};
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  const { user_id } = socket.handshake.auth;
+  users[user_id] = { socket_id: socket.id };
+
+  socket.on('private message', (arg) => {
+    console.log('users:', users);
+    console.log('arg:', arg);
+    const receiver_socket_id = users[arg.to].socket_id;
+    socket
+      .to(receiver_socket_id)
+      .emit('receive private message', { content: arg.content, from: user_id });
+  });
+
+  // Disconnect
+  socket.on('disconnect', () => {
+    delete users[user_id];
+    console.log(`User ${socket.id} disconnected`);
+  });
+});
 
 const connectIndexDb = async () => {
   await databaseService.connect();
@@ -30,6 +64,8 @@ connectIndexDb();
 initFolder();
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/users', usersRouter);
 app.use('/medias', mediasRouter);
 app.use('/static', staticRouter);
@@ -41,4 +77,4 @@ app.use('/search', searchRouter);
 
 app.use(defaultErrorHandler);
 
-app.listen(port);
+httpServer.listen(port);
